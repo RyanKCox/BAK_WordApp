efdd7a0c4feb5bb7736548514834c89946ea5d5f
RyanKCox <ryankcox90@gmail.com>
2022-08-01 10:29:01 -0500

    Finished

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -16,7 +16,11 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.ActivityMainBinding
@@ -26,6 +30,7 @@ import com.example.wordsapp.databinding.ActivityMainBinding
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private var isLinearLayout = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +40,52 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = binding.recyclerView
         // Sets the LinearLayoutManager of the recyclerview
-        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter = LetterAdapter()
+        chooseLayout()
+    }
+
+    private fun toggleLayout(){
+        isLinearLayout = !isLinearLayout
+        chooseLayout()
+    }
+    private fun chooseLayout(){
+        if(isLinearLayout)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        else
+            recyclerView.layoutManager = GridLayoutManager(this,4)
         recyclerView.adapter = LetterAdapter()
+
+    }
+    private fun setIcon(menuItem:MenuItem?){
+        menuItem?.let {
+            it.icon =
+                if(isLinearLayout)
+                    ContextCompat.getDrawable(this,R.drawable.ic_grid_layout)
+                else
+                    ContextCompat.getDrawable(this,R.drawable.ic_linear_layout)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu,menu)
+
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_switch_layout ->{
+                toggleLayout()
+                setIcon(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+//        return super.onOptionsItemSelected(item)
     }
 
 }
